dee294a552e7f3aeb35a8673a923d680a5bcfb30
HeoJiye <heoze1031@gmail.com>
2024-08-12 11:46:07 +0900

    🔥 remove: storybook 불필요한 설정 삭제하기

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,5 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
-import path from 'path';
-import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
-
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
@@ -16,23 +13,6 @@ const config: StorybookConfig = {
   framework: {
     name: '@storybook/nextjs',
     options: {},
-  },
-  typescript: {
-    check: false,
-    checkOptions: {},
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
-    },
-  },
-  webpackFinal: async (config) => {
-    (config.resolve?.plugins ?? []).push(
-      new TsconfigPathsPlugin({
-        configFile: path.resolve(__dirname, '../tsconfig.json'),
-      })
-    );
-    return config;
   },
 };
 export default config;
