34feead4d4db10f6ce35967bd1f65d0b99e174c6
HeoJiye <heoze1031@gmail.com>
2024-08-12 17:40:47 +0900

    🎨 design: SignupForm 컴포넌트 작성

--- src/features/auth/ui/SignupForm.tsx
+++ src/features/auth/ui/SignupForm.tsx
@@ -1,8 +1,27 @@
+import { Button, Input, LinkButton, Logo, Modal } from '@/shard/ui';
+
 export interface SignupFormProps {}
 
 // eslint-disable-next-line no-empty-pattern
 function SignupForm({}: SignupFormProps) {
-  return <div />;
+  return (
+    <Modal>
+      <div className='flex-center flex flex-col gap-48'>
+        <Logo />
+        <div className='flex-center flex flex-col gap-16'>
+          <form className='flex-center flex flex-col gap-36'>
+            <div className='flex-center flex flex-col gap-8'>
+              <Input id='email' name='email' label='이메일' type='text' />
+              <Input id='password' name='password' label='비밀번호' type='password' />
+              <Input id='password comfirm' name='password comfirm' label='비밀번호 확인' type='password' />
+            </div>
+            <Button type='submit'>회원 가입</Button>
+          </form>
+          <LinkButton>로그인하기</LinkButton>
+        </div>
+      </div>
+    </Modal>
+  );
 }
 
 export default SignupForm;
