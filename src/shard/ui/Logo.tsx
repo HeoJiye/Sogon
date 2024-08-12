f85b1596aac10af2dd76728f96352d073c700eb4
HeoJiye <heoze1031@gmail.com>
2024-08-12 16:04:29 +0900

    🎨 design: Logo 컴포넌트 작성

--- src/shard/ui/Logo.tsx
+++ src/shard/ui/Logo.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image';
+
+import IconPath from '@/../public/icon.png';
+
 export interface LogoProps {}
 
 // eslint-disable-next-line no-empty-pattern
 function Logo({}: LogoProps) {
-  return <div data-testid='Logo-component' />;
+  return (
+    <div className='flex-center flex gap-8 text-18 font-bold'>
+      <Image src={IconPath} alt='Logo' width={32} height={32} />
+      소곤
+    </div>
+  );
 }
 
 export default Logo;
