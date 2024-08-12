7f54ef3931ed6d29395b29f2d225b6eaa4fc4225
HeoJiye <heoze1031@gmail.com>
2024-08-12 09:39:11 +0900

    🔧 config: tailwind 설정 수정 + px 단위 default, custom style 몇 가지

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,74 @@
 import type { Config } from 'tailwindcss';
 
+const ROOT_PX = 16;
+
+const px0to100: { [x: number]: string } = {
+  ...Array.from(Array(101)).map((_, i) => `${i / ROOT_PX}rem`),
+};
+const px0to2000: { [x: number]: string } = {
+  ...Array.from(Array(2001)).map((_, i) => `${i / ROOT_PX}rem`),
+};
+
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      width: px0to2000,
+      height: px0to2000,
+      maxWidth: px0to2000,
+      maxHeight: px0to2000,
+      minWidth: px0to2000,
+      minHeight: px0to2000,
+      borderWidth: px0to100,
+      gap: px0to100,
+      padding: px0to100,
+      margin: px0to2000,
+      lineHeight: px0to100,
+      fontSize: px0to100,
+      inset: px0to2000,
+      translate: px0to2000,
+      rotate: px0to2000,
+      skew: px0to2000,
     },
   },
-  plugins: [],
+  plugins: [
+    ({ addComponents }: any) => {
+      const theme = {
+        '.h-w-full': {
+          width: '100%',
+          height: '100%',
+        },
+        '.h-w-screen': {
+          width: '100vw',
+          height: '100vh',
+        },
+        '.flex-center': {
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        '.absolute-center': {
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        },
+        '.absolute-x-center': {
+          left: '50%',
+          transform: 'translateX(-50%)',
+        },
+        '.absolute-y-center': {
+          top: '50%',
+          transform: 'translateY(-50%)',
+        },
+        '.line-clamp-none': {
+          display: '-webkit-box',
+          WebkitBoxOrient: 'vertical',
+          WebkitLineClamp: 1,
+          overflow: 'hidden',
+        },
+      };
+      addComponents(theme);
+    },
+  ],
 };
+
 export default config;
