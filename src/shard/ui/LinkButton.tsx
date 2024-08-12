47caeb5712ec18dd0761ae035fb8d35100f55542
HeoJiye <heoze1031@gmail.com>
2024-08-12 16:49:45 +0900

    🍰 chore: Link -> LinkButton 컴포넌트 이름 변경

--- src/shard/ui/LinkButton.tsx
+++ src/shard/ui/LinkButton.tsx
@@ -1,10 +1,10 @@
-export interface LinkProps {
+export interface LinkButtonProps {
   onClick?: () => void;
   children: React.ReactNode;
 }
 
 // eslint-disable-next-line no-empty-pattern
-function Link({ onClick, children }: LinkProps) {
+function LinkButton({ onClick, children }: LinkButtonProps) {
   return (
     <button className='btn btn-link font-thin text-neutral hover:scale-105' type='button' onClick={onClick}>
       {children}
@@ -12,4 +12,4 @@ function Link({ onClick, children }: LinkProps) {
   );
 }
 
-export default Link;
+export default LinkButton;
