92e3253a3d23dc70a82310abb03c0919e4b1f800
HeoJiye <heoze1031@gmail.com>
2024-08-12 16:39:57 +0900

    🍰 chore: LoginForm, SignupForm 컴포넌트 템플릿 생성

---
+++ src/features/auth/ui/LoginForm.tsx
@@ -0,0 +1,8 @@
+export interface LoginFormProps {}
+
+// eslint-disable-next-line no-empty-pattern
+function LoginForm({}: LoginFormProps) {
+  return <div />;
+}
+
+export default LoginForm;
---
+++ src/features/auth/ui/SignupForm.tsx
@@ -0,0 +1,8 @@
+export interface SignupFormProps {}
+
+// eslint-disable-next-line no-empty-pattern
+function SignupForm({}: SignupFormProps) {
+  return <div />;
+}
+
+export default SignupForm;
