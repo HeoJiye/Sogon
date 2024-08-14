eecf4693cffc11a459936d34520f02c06c288b4f
HeoJiye <heoze1031@gmail.com>
2024-08-14 13:40:42 +0900

    🎨 design: alert 커스텀 애니메이션 삭제, 확인 버튼 -> x 버튼으로 수정

--- src/shard/ui/Alert.item.tsx
+++ src/shard/ui/Alert.item.tsx
@@ -1,7 +1,6 @@
-import { CheckIcon, InformationCircleIcon, XCircleIcon } from '@heroicons/react/24/outline';
+import { CheckIcon, InformationCircleIcon, XCircleIcon, XMarkIcon } from '@heroicons/react/24/outline';
 
 import cn from 'classnames';
-import { useEffect, useState } from 'react';
 
 interface AlertItemProps {
   type: 'error' | 'success' | 'info';
@@ -11,43 +10,19 @@ interface AlertItemProps {
 }
 
 function AlertItem({ type, message, persistent, onClose }: AlertItemProps) {
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    setIsVisible(true);
-
-    if (!persistent) {
-      setTimeout(() => {
-        setIsVisible(false);
-        setTimeout(onClose, 400);
-      }, 3000);
-    }
-  }, [persistent, onClose]);
-
-  const handleClose = () => {
-    setIsVisible(false);
-    setTimeout(onClose, 500);
-  };
-
   return (
     <div
-      className={cn('alert w-fit min-w-400 transform transition-all ease-in-out', {
+      className={cn('alert w-fit min-w-400 text-14', {
         'alert-error': type === 'error',
         'alert-success': type === 'success',
         'alert-info': type === 'info',
-        'translate-y-20 rotate-12 scale-75 opacity-0 duration-500': !isVisible,
-        'translate-y-0 rotate-0 scale-110 opacity-100 duration-700': isVisible,
       })}
     >
       {type === 'error' && <XCircleIcon className='h-24 w-24' />}
       {type === 'success' && <CheckIcon className='h-24 w-24' />}
       {type === 'info' && <InformationCircleIcon className='h-24 w-24' />}
       <span className='ml-2'>{message}</span>
-      {persistent && (
-        <button className='btn btn-ghost btn-sm ml-4' type='button' onClick={handleClose}>
-          확인
-        </button>
-      )}
+      {persistent && <XMarkIcon className='h-18 w-18 cursor-pointer' onClick={onClose} />}
     </div>
   );
 }
