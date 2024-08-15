8d4a0daae4da79f61f298ada28e256faf797ca45
HeoJiye <heoze1031@gmail.com>
2024-08-15 22:02:54 +0900

    ♻️ refactor: token 검사, 이메일 인증 여부 미들웨어로 분리

--- src/shard/lib/middleware.auth.ts
+++ src/shard/lib/middleware.auth.ts
@@ -1,10 +1,10 @@
-import { type NextRequest, NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 import { auth } from '@/shard/lib/firebaseAdmin';
 import { ForbiddenError, InternalServerError, UnauthorizedError } from '@/shard/model/errors/APIErrors';
 import { FIREBASE_AUTH_ERROR, isFirebaseAuthError } from '@/shard/model/errors/firebaseErrors';
 
-async function verifyToken(request: NextRequest) {
+export async function tokenMiddleware(request: NextRequest, next: () => symbol) {
   const token = request.cookies.get('token')?.value;
 
   if (!token) {
@@ -14,7 +14,6 @@ async function verifyToken(request: NextRequest) {
   try {
     const user = await auth.verifyIdToken(token);
     request.cookies.set('x-uid', user.uid);
-    return user;
   } catch (error) {
     if (!isFirebaseAuthError(error)) {
       return new InternalServerError('인증 검증 과정에서 알 수 없는 오류가 발생했습니다.').toResponse();
@@ -31,25 +30,18 @@ async function verifyToken(request: NextRequest) {
     }
     return new UnauthorizedError('토큰이 유효하지 않습니다. 다시 로그인해주세요.').toResponse();
   }
-}
-
-export async function tokenMiddlewareWithoutEmailVerified(request: NextRequest, next: () => symbol) {
-  const user = await verifyToken(request);
-  if (user instanceof NextResponse) {
-    return user;
-  }
-
   return next();
 }
 
-export async function tokenMiddleware(request: NextRequest, next: () => symbol) {
-  const user = await verifyToken(request);
-  if (user instanceof NextResponse) {
-    return user;
-  }
+export async function emailVerifiedMiddleware(request: NextRequest, next: () => symbol) {
+  const uid = request.cookies.get('x-uid')?.value;
 
-  const createUserAPI = request.method === 'POST' && request.nextUrl.pathname === '/api/users';
-  if (!createUserAPI && user.email_verified === false) {
+  if (!uid) {
+    return new InternalServerError('서버에 문제가 있습니다. 잠시 후 다시 시도해주세요.').toResponse();
+  }
+  const user = await auth.getUser(uid);
+
+  if (user.emailVerified === false) {
     return new ForbiddenError('이메일 인증이 필요합니다.').toResponse();
   }
 
