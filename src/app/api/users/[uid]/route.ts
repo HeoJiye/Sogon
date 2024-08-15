d4da89bcf0446e3f95afed2228ad78a332038f7f
HeoJiye <heoze1031@gmail.com>
2024-08-16 00:22:46 +0900

    ✨ feat: 유저 프로필 조회 API 구현

--- src/app/api/users/[uid]/route.ts
+++ src/app/api/users/[uid]/route.ts
@@ -1,6 +1,13 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-import { type CreateProfileDTO, USER_RECORD, type User, createProfileSchema } from '@/entities/user/model';
+import {
+  type CreateProfileDTO,
+  USER_RECORD,
+  type User,
+  ViewProfileDTO,
+  createProfileSchema,
+  viewProfileSchema,
+} from '@/entities/user/model';
 import { db } from '@/shard/lib/firebaseAdmin';
 import {
   UID_HEADER_FIELD,
@@ -50,4 +57,64 @@ export const PUT = handler(
   emailVerifiedMiddleware,
   validateMiddleware(createProfileSchema),
   updateProfile
+);
+
+async function getFriendStatus(currentUserId: string, userId: string) {
+  if (currentUserId === userId) {
+    return 'self';
+  }
+
+  const friendsRef = db.collection('users').doc(currentUserId).collection('friends').doc(userId);
+  const friendDoc = await friendsRef.get();
+
+  if (friendDoc.exists) {
+    return 'friends';
+  }
+  const friendRequestRef1 = db.collection('users').doc(currentUserId).collection('friendRequests').doc(userId);
+  const friendRequestDoc1 = await friendRequestRef1.get();
+
+  const friendRequestRef2 = db.collection('users').doc(currentUserId).collection('friendRequests').doc(userId);
+  const friendRequestDoc2 = await friendRequestRef2.get();
+
+  if (friendRequestDoc1.exists || friendRequestDoc2.exists) {
+    return 'pending';
+  }
+  return 'none';
+}
+
+async function getUserProfile(request: NextRequest, { params }: { params: { uid: string } }) {
+  const currentUserId = request.headers.get(UID_HEADER_FIELD);
+  const userId = params.uid;
+
+  if (!currentUserId) {
+    return new InternalServerError('인증 미들웨어에 문제가 있습니다. 개발자에게 문의해주세요.').toResponse();
+  }
+
+  const userRef = db.collection(USER_RECORD).doc(userId);
+  const doc = await userRef.get();
+  if (!doc.exists) {
+    return new NotFoundError('해당 사용자 프로필을 찾을 수 없습니다.').toResponse();
+  }
+
+  const userData = doc.data();
+
+  const status = await getFriendStatus(currentUserId, userId);
+
+  return NextResponse.json(
+    {
+      userId,
+      nickname: userData?.nickname,
+      profileImage: userData?.profileImage,
+      bio: userData?.bio,
+      status,
+    } satisfies ViewProfileDTO,
+    { status: 200 }
+  );
+}
+
+export const GET = handler(
+  tokenMiddleware,
+  emailVerifiedMiddleware,
+  validateMiddleware(viewProfileSchema),
+  getUserProfile
 );
--- src/entities/user/model/dto.ts
+++ src/entities/user/model/dto.ts
@@ -11,3 +11,19 @@ export const createProfileSchema = z.object({
   profileImage: z.string().url().optional(),
   bio: z.string().max(50).nullable(),
 });
+
+export type ViewProfileDTO = {
+  userId: string;
+  nickname: string;
+  profileImage: string;
+  bio: string;
+  status: 'self' | 'friends' | 'pending' | 'none';
+};
+
+export const viewProfileSchema = z.object({
+  userId: z.string(),
+  nickname: z.string(),
+  profileImage: z.string().url(),
+  bio: z.string(),
+  status: z.union([z.literal('self'), z.literal('friends'), z.literal('pending'), z.literal('none')]),
+});
