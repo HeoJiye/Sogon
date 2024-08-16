50d3340a420aca9226aa7ac116d7875aa3c6afc1
HeoJiye <heoze1031@gmail.com>
2024-08-16 18:01:01 +0900

    ✨ feat: 친구 조회 API 작성

---
+++ src/features/friend/model/dto.ts
@@ -0,0 +1,5 @@
+export type FriendResponseDTO = {
+  userId: string;
+  nickname: string;
+  profileImage: string | null;
+};
--- src/features/friend/model/schema.ts
+++ src/features/friend/model/schema.ts
@@ -1,3 +1,11 @@
+export const FRIEND_RECORD = 'friends' as const;
+
+export type Friend = {
+  createdAt: Date;
+};
+
+export const FRIEND_REQUEST_RECORD = 'friendRequests' as const;
+
 export type FriendRequest = {
   senderId: string;
   message: string;
