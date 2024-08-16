f4ca9c2d0134a7dd7526b439fb5c227eea59a411
HeoJiye <heoze1031@gmail.com>
2024-08-16 10:59:42 +0900

    🔥 remove: 안쓰는 코드 삭제

--- src/shard/model/errors/firebaseErrors.ts
+++ src/shard/model/errors/firebaseErrors.ts
@@ -1,7 +1,1 @@
-import { FirebaseError } from 'firebase/app';
-
 export * from './firebaseErrors.auth';
-
-export function isFirebaseError(error: unknown): error is FirebaseError {
-  return error instanceof FirebaseError;
-}
