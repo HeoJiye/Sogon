326b6c803fec3dbfc84f2be58178f91843eff27e
HeoJiye <heoze1031@gmail.com>
2024-08-17 22:22:20 +0900

    ♻️ refactor: commnet 추가, 삭제 시 post에 comment count 추가하기

--- src/features/comment/service/createComment.ts
+++ src/features/comment/service/createComment.ts
@@ -1,6 +1,8 @@
 import { Timestamp } from 'firebase-admin/firestore';
 
+import { Post } from '@/entities/post/model';
 import { getPostRefById } from '@/entities/post/service';
+import { db } from '@/shard/lib/firebaseAdmin';
 
 import { COMMENT_RECORD, Comment, EditCommentRequestDTO, EditCommentResponseDTO } from '../model';
 
@@ -10,15 +12,24 @@ export async function createComment(
   { content }: EditCommentRequestDTO
 ): Promise<EditCommentResponseDTO> {
   const postRef = await getPostRefById(postId);
+  const commentRef = postRef.collection(COMMENT_RECORD).doc();
 
   const timestamp = Timestamp.fromDate(new Date());
 
-  const commentRef = await postRef.collection(COMMENT_RECORD).add({
-    authorId: userId,
-    content,
-    createdAt: timestamp,
-    updatedAt: timestamp,
-  } satisfies Comment);
+  await db.runTransaction(async (transaction) => {
+    const post = (await transaction.get(postRef)).data() as Post;
+
+    transaction.update(postRef, {
+      commentCount: post.commentCount + 1,
+    } satisfies Partial<Post>);
+
+    transaction.create(commentRef, {
+      authorId: userId,
+      content,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    } satisfies Comment);
+  });
 
   const editedComment = (await commentRef.get()).data() as Comment;
 
