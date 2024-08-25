c8ba89017596de0681ac617ee32c6e9db45d5c25
HeoJiye <heoze1031@gmail.com>
2024-08-25 20:59:04 +0900

    ✨ feat: IconButton onClick 추가

--- src/shared/ui/IconButton.tsx
+++ src/shared/ui/IconButton.tsx
@@ -1,10 +1,15 @@
 export interface IconButtonProps {
   icon: React.ReactNode;
+  onClick?: () => void;
 }
 
-function IconButton({ icon }: IconButtonProps) {
+function IconButton({ icon, onClick }: IconButtonProps) {
   return (
-    <button type='button' className='animate-btn h-fit w-fit cursor-pointer rounded-full p-[2%] hover:bg-base-200'>
+    <button
+      type='button'
+      className='animate-btn h-fit w-fit cursor-pointer rounded-full p-[2%] hover:bg-base-200'
+      onClick={() => onClick?.()}
+    >
       {icon}
     </button>
   );
