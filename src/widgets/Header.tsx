1b4dcab218faf9566cc020b721c8fe6e71eb29e7
HeoJiye <heoze1031@gmail.com>
2024-08-26 14:44:01 +0900

    ♻️ refactor: header 네비게이션 수정

--- src/widgets/Header.tsx
+++ src/widgets/Header.tsx
@@ -3,9 +3,10 @@
 import {
   ArrowRightStartOnRectangleIcon,
   BellIcon,
-  HomeIcon,
+  EnvelopeIcon,
+  NewspaperIcon,
+  PencilSquareIcon,
   UserCircleIcon,
-  UsersIcon,
 } from '@heroicons/react/24/outline';
 
 import Link from 'next/link';
@@ -28,18 +29,19 @@ function Header({}: HeaderProps) {
   return (
     <header className='fixed z-50 flex h-55 w-full items-center justify-between bg-neutral p-24'>
       <Logo title={false} />
-      <nav className='flex h-fit gap-20'>
-        <Link href='/'>
-          <HomeIcon className='h-28 w-28 cursor-pointer text-base-100' />
-        </Link>
+      <nav className='flex h-fit gap-24'>
         <Link href={`/profile/${getCurUID()}`}>
           <UserCircleIcon className='h-28 w-28 cursor-pointer text-base-100' />
         </Link>
-        <Link href='/friends'>
-          <UsersIcon className='h-28 w-28 cursor-pointer text-base-100' />
+        <Link href='/'>
+          <NewspaperIcon className='h-28 w-28 cursor-pointer text-base-100' />
+        </Link>
+        <Link href='/edit-post'>
+          <PencilSquareIcon className='h-28 w-28 cursor-pointer text-base-100' />
         </Link>
       </nav>
       <div className='flex h-fit gap-20'>
+        <EnvelopeIcon className='h-28 w-28 cursor-pointer text-base-100' />
         <BellIcon className='h-28 w-28 cursor-pointer text-base-100' />
         <ArrowRightStartOnRectangleIcon className='h-28 w-28 cursor-pointer text-base-100' onClick={handleLogout} />
       </div>
