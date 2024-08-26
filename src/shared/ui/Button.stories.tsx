801438bf88cca74c2d6e0707f40cb0aa6631b21b
HeoJiye <heoze1031@gmail.com>
2024-08-26 09:38:29 +0900

    🎨 design: Button 사이즈 추가 + story 작성

--- src/shared/ui/Button.stories.tsx
+++ src/shared/ui/Button.stories.tsx
@@ -23,3 +23,23 @@ export const Default: Story = {
 export const LongText: Story = {
   args: { children: '버튼버튼버튼버튼버튼버튼버튼버튼버튼' },
 };
+
+export const Cancel: Story = {
+  args: { children: '버튼', color: 'cancel' },
+};
+
+export const Disabled: Story = {
+  args: { children: '버튼', color: 'disabled' },
+};
+
+export const Small: Story = {
+  args: { children: '버튼', size: 's' },
+};
+
+export const Medium: Story = {
+  args: { children: '버튼', size: 'm' },
+};
+
+export const Skeleton: Story = {
+  args: { children: '버튼', skeleton: true },
+};
